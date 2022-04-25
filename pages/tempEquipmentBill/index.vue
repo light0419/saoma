8a7d00f971251bd28e5ab311f595fa5be329b9ca
DESKTOP-ON9UIMA\admin <763198261@qq.com>
2022-04-25 18:34:09 +0800

    1

--- pages/tempEquipmentBill/index.vue
+++ pages/tempEquipmentBill/index.vue
@@ -1,6 +1,11 @@
 <template>
 	<view class="content">
-		
+		<view class="top">
+			<image src="../../static/saoma.png" mode="widthFix"></image>
+			<view class="word">
+				点击扫码入库
+			</view>	
+		</view>
 	</view>
 </template>
 
@@ -28,5 +33,15 @@
 </script>
 
 <style lang="less">
-	
+	.content{
+		background:url(@/static/banyuan.png) no-repeat center top;
+		background-size:750rpx;
+		.top{
+			border-radius: 20rpx;
+			background-color: rgb( 255, 255, 255 );
+			box-shadow: 1.5rpx 2.598rpx 7rpx 0rpx rgb( 181, 179, 177 );
+			width: 667rpx;
+			height: 222rpx;
+		}
+	}
 </style>
